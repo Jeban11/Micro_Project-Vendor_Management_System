35b5a231d7f76dfd2e827e1ab7c7f91b1be0a4a7
Jeban Ignesh J <142471481+Jeban11@users.noreply.github.com>
2024-02-09 00:18:17 +0530

    Delete BackEnd directory

--- BackEnd/SB-VMS/src/main/java/com/vms/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.vms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vms.bean.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long>{
-
-}
