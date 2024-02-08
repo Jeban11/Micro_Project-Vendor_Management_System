2d6343528b692585eaf0f179579e87e4f6545985
Jeban Ignesh J <142471481+Jeban11@users.noreply.github.com>
2024-02-08 12:17:56 +0530

    Delete BackEnd/SB-VMS directory

--- BackEnd/SB-VMS/src/main/java/com/vms/dao/ProductDAO.java
+++
@@ -1,9 +0,0 @@
-package com.vms.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vms.bean.Product;
-
-public interface ProductDAO extends JpaRepository<Product, Long>{
-
-}
